9ca5f4d5e886e59f91c7ed5251b688b500ffde2f
Sayush36 <ayushds2003@gmail.com>
2024-12-10 16:18:00 +0530

    Add files via upload

---
+++ helloarray.js
@@ -0,0 +1,11 @@
+const studentName=["Mohit","Rohit","Bipul"];
+console.log(studentName);
+const studentname1=[];
+studentname1[0]="mohit";
+studentname1[1]="sahil";
+studentname1[2]="rohir=t";
+console.log(studentname1);
+for(let i=0;studentName.length;i++)
+{
+    console.log(studentName[i]);
+}
